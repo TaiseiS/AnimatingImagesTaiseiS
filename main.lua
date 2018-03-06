91a139d2409df08de89a092dc873a0227df6ea58
TaiseiS <taisei.scott@ocsbstudent.ca>
2018-03-06 10:16:19 -0500

    Initial commit

---
+++ main.lua
@@ -0,0 +1,9 @@
+-----------------------------------------------------------------------------------------
+-- Animating Images
+-- Taisei Scott
+-- This pogram displays 3 images moving in different directions with\
+-- some coloured text and background.
+--
+-----------------------------------------------------------------------------------------
+
+-- Your code here
