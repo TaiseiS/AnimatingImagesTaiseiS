f78ceffcb27bfbc0273de564d5de45e5c525acd4
TaiseiS <taisei.scott@ocsbstudent.ca>
2018-03-20 01:30:38 -0400

    Yes
    New Stuff

--- main.lua
+++ main.lua
@@ -6,4 +6,19 @@
 --
 -----------------------------------------------------------------------------------------
 
--- Your code here
+--hide status bar
+display.setStatusBar(display.HiddenStatusBar)
+
+--set background
+local background = display.newImageRect("Images/GARBAGE.jpg", display.contentWidth, display.contentHeight)
+background.anchorX = 0
+background.anchorY = 0
+
+--audio
+local bgm = audio.loadStream("Audio/Death By Glamour.mp3")
+--audio.play ( bgm, { channel=1, loops=-1, fadein=5000 })
+
+--text
+local text = display.newText("wow very quality yes", 500, 600, nil, 35)
+text:setTextColor(0.2, 1, 0.3)
+
